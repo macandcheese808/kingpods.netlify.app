2ae9307aad9ac46832f5252d8d9d6724129b4976
Ice Cubenetix <macandcheese808@icloud.com>
2023-06-02 22:38:23 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Welcome To KingPods!");
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
