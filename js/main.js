d21c444d688d05b6b303c89988555bf561c1cc71
Ice Cubenetix <macandcheese808@icloud.com>
2023-06-03 14:23:50 +0200

    update

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+var loader = document.getElementById('preloader');
+
+window.addEventListener('load', function() {
+    loader.style.display = 'none'
+})
