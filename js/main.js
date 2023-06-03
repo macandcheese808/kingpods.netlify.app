0ce3e6c2cb6ef671645dfae9d859a5dce4f5bb98
Ice Cubenetix <macandcheese808@icloud.com>
2023-06-03 15:14:48 +0200

    updaet

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-console.log("Welcome To KingPods!");
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-  
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-var loader = document.getElementById('preloader');
-
-window.addEventListener('load', function() {
-    loader.style.display = 'none'
-})
+function openNav(){document.getElementById("mySidenav").style.width="250px"}function closeNav(){document.getElementById("mySidenav").style.width="0"}console.log("Welcome To KingPods!");var loader=document.getElementById("preloader");window.addEventListener("load",function(){loader.style.display="none"});
